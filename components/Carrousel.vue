0ca224c55fe8807cd84d7b35915e90c5598f1349
dracochen1 <noedubois41@gmail.com>
2021-03-21 15:24:33 +0100

    Homepageok

--- components/Carrousel.vue
+++ components/Carrousel.vue
@@ -1,10 +1,77 @@
 <template>
-  <div class="carousel-wrapper">
-    <VueSlickCarousel v-bind="slickOptions">
-      <div v-for="i in 4" :key="i" class="img-wrapper">
-        <img :src="`./${i}-200x100.jpg`" />
-      </div>
-    </VueSlickCarousel>
+  <div>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="650"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <!-- Text slides with image -->
+      <b-carousel-slide>
+        <template #img>
+          <img
+            width="1024"
+            height="650"
+            src="https://static0.srcdn.com/wordpress/wp-content/uploads/2020/11/superman-angry-featured.jpg"
+            alt="Superman"
+          >
+        </template>
+      </b-carousel-slide>
+      <!-- Slides with custom text -->
+      <b-carousel-slide>
+        <template #img>
+          <img
+            width="1024"
+            height="650"
+            src="https://static3.srcdn.com/wordpress/wp-content/uploads/2020/05/Batman-Jason-Fabok-Comic-Art.jpg"
+            alt="Batman"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <!-- Slides with image only -->
+      <b-carousel-slide>
+        <template #img>
+          <img
+            width="1024"
+            height="650"
+            src="https://c.wallhere.com/photos/b8/b4/1920x984_px_DC_Comics_Supergirl_superhero-1094145.jpg!d"
+            alt="Supergirl"
+          >
+        </template>
+      </b-carousel-slide>
+
+      <b-carousel-slide>
+        <template #img>
+          <img
+            width="1024"
+            height="650"
+            src="https://www.disneyphile.fr/wp-content/uploads/2021/02/thumb2-deadpool-4k-superheroes-art-gun.jpg"
+            alt="Deadpool"
+          >
+        </template>
+      </b-carousel-slide>
+      <!-- Slides with img slot -->
+      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+      <b-carousel-slide>
+        <template #img>
+          <img
+            width="1024"
+            height="650"
+            src="https://images.hdqwalls.com/wallpapers/the-boys-season-2-x6.jpg"
+            alt="The seven"
+          >
+        </template>
+      </b-carousel-slide>
+
+    </b-carousel>
   </div>
 </template>
 
@@ -12,35 +79,18 @@
 export default {
   data() {
     return {
-      slickOptions: {
-        slidesToShow: 3,
-        arrows: false
-      }
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true
+    },
+    onSlideEnd(slide) {
+      this.sliding = false
     }
   }
 }
 </script>
 
-<style>
-.carousel-wrapper {
-  padding: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-h3{
-    color:white;
-    text-align: center;
-    margin-right: 25px;
-}
-.img-wrapper{
-  display:flex;
-  flex-direction: row;
-}
-.img-wrapper img {
-  width: 200px;
-  height: 100px;
-  background-image: linear-gradient(gray 100%, transparent 0);
-  margin-top:5px;
-}
-</style>
