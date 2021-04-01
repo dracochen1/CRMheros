5ce13bd2e631355100e397b05fa58f30f3c09c94
dracochen1 <noedubois41@gmail.com>
2021-04-01 17:32:30 +0200

    Nameoforga

--- pages/admin/civils/post.vue
+++ pages/admin/civils/post.vue
@@ -65,7 +65,12 @@
             {{ organisation.id }}
           </option>
         </select>
-        <span style="padding-left:5%">Organisation is: {{form.organization}}</span>
+      </div>
+      <div class="form-group">
+        <label for="organisations">Civil organisation name is : </label>
+          <ul v-for="organisation in organisations" v-bind:key="organisation.id">
+            <li>{{ organisation.name }}</li>
+          </ul>
       </div>
       <div class="form-group">
         <button class="btn btn-primary" onclick="alert('Civil ajouté!')">Submit</button>
@@ -154,5 +159,11 @@ h2{
 }
 span{
   color:white;
+}
+li{
+  color:white;
+}
+ul {
+  list-style-type: none;
 }
 </style>
