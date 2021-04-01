864aba78e7124bbb0d6417d20abe398dbc3f3d80
Yasmine <yasmine.bouslimani@gmail.com>
2021-04-01 20:24:20 +0200

    Change landing page path to admin-account

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <h4 v-if="error.statusCode === 404" class="text-center text-white">Bonjour citoyen, cette page n'existe pas.</h4>
-    <h4 class="text-center text-white" v-else>Bonjour citoyen, une erreur est vite arrivée. Vous pouvez retournez à la page d'accueil.</h4>
+    <h4 class="text-center text-white" v-else>Bonjour citoyen, vous n'êtes pas autorisé ici.</h4>
     <h5 class="text-center text-white">Vous pouvez retournez à la
-      <NuxtLink to="/admin"> page d'accueil</NuxtLink>
+      <NuxtLink to="/admin/account"> page d'accueil</NuxtLink>
     </h5>
     <p class="text-center text-white">La Hero Corp.</p>
 
